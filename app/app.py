86f0121746e4db6f513ac31f351a8e2cda37664a
wsy0418 <146824307+wsy0418@users.noreply.github.com>
2023-10-21 21:25:27 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -14,10 +14,10 @@ def hello_world():
     else:
         myage = request.form['age']
         mygender = request.form['gender']
-        model = load('app/bag-recommender.joblib')
+        model = load('app/phone-recommender.joblib')
         np_arr = np.array([myage, mygender])
         predictions = model.predict([np_arr])  
         predictions_to_str = str(predictions)
         #return predictions_to_str
-        return render_template('index.html', href2='The suitable bag for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
+        return render_template('index.html', href2='The suitable phone for you (age:'+str(myage)+' ,gender:'+str(mygender)+') is:'+predictions_to_str)
 
